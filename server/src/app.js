dc8b90cddc23b1c29854735f927f2a0f46de8310
AlexKutkanych <alexander.kutkanych@gmail.com>
2023-01-10 16:23:39 +0100

    feat: added swagger

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const api = require('./routes/api');
 
@@ -16,6 +18,7 @@ app.use(
 // app.use(morgan('combined'));
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // serve built React app
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', api);
